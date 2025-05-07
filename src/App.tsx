27669de158c1986388a29f9fd24172564b41eef6
bisal-xetri <bishaldkl56@gmail.com>
2025-05-07 18:30:28 +0545

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import Layout from "./components/Layout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "./context/theme-provider";
+import WeatherDashboard from "./pages/WeatherDashboard";
+import CityPage from "./pages/CityPage";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Layout>
+          <Routes>
+            <Route path="/" element={<WeatherDashboard />} />
+            <Route path="/city/:cityName" element={<CityPage />} />
+          </Routes>
+        </Layout>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,20 @@
+import type { PropsWithChildren } from "react";
+import Header from "./Header";
+
+const Layout = ({ children }: PropsWithChildren) => {
+  return (
+    <div className="bg-gradient-to-br from-background to-muted ">
+      <Header />
+      <main className="min-h-screen container mx-auto px-4 py-8">
+        {children}
+      </main>
+      <footer className="border-t backdrop-blur">
+        <div className="container mx-auto px-4 py-12  text-center text-gray-400">
+          <p>Made with ❤️ by Bishal Dai</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
